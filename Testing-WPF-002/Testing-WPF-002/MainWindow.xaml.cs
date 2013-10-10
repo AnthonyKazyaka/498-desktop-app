e4977c35af5218af241d606b67844bcf5d67ddb1
David <dvdjones08@yahoo.com>
2013-10-10 00:19:49 -0400

    Initial Commit
    VS2010 project for sending Tin Can statements to our server.

---
+++ Testing-WPF-002/Testing-WPF-002/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Testing_WPF_002
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string userLogin;
+        private string userPassword;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click_1(object sender, RoutedEventArgs e)
+        {
+            userLogin = UserField.Text;
+            userPassword = passwordBox1.Password;
+            TinCan.ConnectToTinCan(userLogin, userPassword);
+        }
+    }
+}
---
+++ Testing-WPF-002/Testing-WPF-002/TinCan.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RusticiSoftware.TinCanAPILibrary;
+using RusticiSoftware.TinCanAPILibrary.Model;
+
+namespace Testing_WPF_002
+{
+    class TinCan
+    {
+        private static bool tri = false;
+
+        public static void ConnectToTinCan(string usrName, string passwd)
+        {
+            try
+            {
+                TCAPI tincan = new TCAPI(new Uri("http://35.9.22.105:8000/xapi"), new BasicHTTPAuth(usrName, passwd),
+                    RusticiSoftware.TinCanAPILibrary.Helper.TCAPIVersion.TinCan1p0p0);
+            }
+
+        }
+
+        public static void SendStatement()
+        {
+            //TODO
+        }
+    }
+}
